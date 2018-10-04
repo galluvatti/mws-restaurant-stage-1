b92745dbae4a4c2948ecaf25cb101a9be82f9b30
galluvatti <giovanni.alluvatti@gmail.com>
2018-10-04 17:15:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiZ2FsbHV2YXR0aSIsImEiOiJjamlvY3BoOWIwZmViM2txc3liYmN1dzgxIn0.XsLpEWpSuB9wVf-JBDNfeA',
+    mapboxToken: 'YOUR_API_KEY',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
